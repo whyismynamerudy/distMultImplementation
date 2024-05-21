112630db9503dc021b8ea89682d8ef44c513b5d3
Rudy <rudraksh.monga@mail.utoronto.ca>
2024-05-20 22:40:27 -0400

    trying adding more samples

--- dataloader.py
+++ dataloader.py
@@ -4,6 +4,28 @@ Handles reading in Knowledge Graph and creates a dataloader for it.
 
 import torch
 from torch.utils.data import Dataset
+import numpy as np
+
+
+# from https://github.com/DeepGraphLearning/KnowledgeGraphEmbedding/blob/master/codes/dataloader.py#L96
+def _get_true_head_and_tail(triples):
+    true_head = {}
+    true_tail = {}
+
+    for head, relation, tail in triples:
+        if (head, relation) not in true_tail:
+            true_tail[(head, relation)] = []
+        true_tail[(head, relation)].append(tail)
+        if (relation, tail) not in true_head:
+            true_head[(relation, tail)] = []
+        true_head[(relation, tail)].append(head)
+
+    for relation, tail in true_head:
+        true_head[(relation, tail)] = np.array(list(set(true_head[(relation, tail)])))
+    for head, relation in true_tail:
+        true_tail[(head, relation)] = np.array(list(set(true_tail[(head, relation)])))
+
+    return true_head, true_tail
 
 
 def load_dict(dict_file):
@@ -37,10 +59,12 @@ def load_triples(txt_file, entity2id, relation2id):
 
 
 class TrainDataLoader(Dataset):
-    def __init__(self, triples, num_entities):
+    def __init__(self, triples, num_entities, negative_sample_size=128):
         self.triples = triples
         self.triples_set = set(triples)
         self.num_entities = num_entities
+        self.negative_sample_size = negative_sample_size
+        self.true_head, self.true_tail = _get_true_head_and_tail(triples)
 
     def __len__(self):
         return len(self.triples)
@@ -49,28 +73,31 @@ class TrainDataLoader(Dataset):
         """
         Return positive sample corresponding to idx with negative sample.
 
-        In the paper, they sample 2 negative triplets (one head, one tail) for each positive sample.
+        In the paper, they sample negative triplets (head, tail) for each positive sample.
 
         :param idx: Index of desired triplet.
-        :return: Tensors pos_sample [1, 3], neg_head [1], and neg_tail [1].
+        :return:
         """
         positive_sample = self.triples[idx]  # positive sample
 
-        neg_head, neg_tail = list(positive_sample[:]), list(positive_sample[:])
-        random_head = int(torch.randint(low=0, high=self.num_entities, size=(1,)).flatten())
-        random_tail = int(torch.randint(low=0, high=self.num_entities, size=(1,)).flatten())
+        heads = torch.LongTensor(self.random_sampling(self, positive_sample, True))
+        tails = torch.LongTensor(self.random_sampling(self, positive_sample, False))
 
-        neg_head[0] = random_head
-        while set(neg_head) in self.triples_set:
-            random_head = int(torch.randint(low=0, high=self.num_entities, size=(1,)).flatten())
-            neg_head[0] = random_head
+        # neg_head, neg_tail = list(positive_sample[:]), list(positive_sample[:])
+        # random_head = int(torch.randint(low=0, high=self.num_entities, size=(1,)).flatten())
+        # random_tail = int(torch.randint(low=0, high=self.num_entities, size=(1,)).flatten())
+        #
+        # neg_head[0] = random_head
+        # while set(neg_head) in self.triples_set:
+        #     random_head = int(torch.randint(low=0, high=self.num_entities, size=(1,)).flatten())
+        #     neg_head[0] = random_head
+        #
+        # neg_tail[2] = random_tail
+        # while set(neg_tail) in self.triples_set:
+        #     random_tail = int(torch.randint(low=0, high=self.num_entities, size=(1,)).flatten())
+        #     neg_tail[2] = random_tail
 
-        neg_tail[2] = random_tail
-        while set(neg_tail) in self.triples_set:
-            random_tail = int(torch.randint(low=0, high=self.num_entities, size=(1,)).flatten())
-            neg_tail[2] = random_tail
-
-        return positive_sample, (random_head, random_tail)
+        return torch.LongTensor(positive_sample), (heads, tails)
 
     @staticmethod
     def collate_fn(batch):
@@ -81,10 +108,40 @@ class TrainDataLoader(Dataset):
         neg_tails = [sample[1][1] for sample in batch]
 
         pos = torch.stack([torch.tensor(_) for _ in pos_samples])       # [N, 3]
-        neg_head = torch.stack([torch.tensor(_) for _ in neg_heads])    # [N]
-        neg_tail = torch.stack([torch.tensor(_) for _ in neg_tails])    # [N]
+        neg_head = torch.stack([torch.tensor(_) for _ in neg_heads])    # [N, num_neg_sample]
+        neg_tail = torch.stack([torch.tensor(_) for _ in neg_tails])    # [N, num_neg_sample]
 
         return pos, (neg_head, neg_tail)
+
+    @staticmethod
+    def random_sampling(self, positive_sample, head=True):
+        head, relation, tail = positive_sample
+        all_entities = np.arange(0, self.num_entities - 1)
+
+        if head:
+            mask = np.isin(
+                all_entities,
+                self.true_head[(relation, tail)],
+                assume_unique=True,
+                invert=True
+            )
+        else:
+            mask = np.isin(
+                all_entities,
+                self.true_tail[(head, relation)],
+                assume_unique=True,
+                invert=True
+            )
+
+        negative_sample_pool = all_entities[mask]
+
+        negative_samples = np.random.choice(
+            negative_sample_pool,
+            size=min(len(negative_sample_pool), self.negative_sample_size),
+            replace=False
+        )
+
+        return negative_samples
 
 
 class TestDataLoader(Dataset):
